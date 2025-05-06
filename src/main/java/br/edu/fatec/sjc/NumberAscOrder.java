98328a542d1f9b6046d318e9734e2b00a6fc53e6
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2025-05-05 21:00:52 -0300

    added class and test

---
+++ src/main/java/br/edu/fatec/sjc/NumberAscOrder.java
@@ -0,0 +1,29 @@
+package br.edu.fatec.sjc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NumberAscOrder <T extends Number & Comparable<T>> {
+
+    private List<T> numbers;
+
+    public NumberAscOrder(CustomStack<T> customStack) throws StackEmptyException {
+        this.numbers = new ArrayList<T>();
+        while (!customStack.isEmpty()) {
+            T number = customStack.pop();
+            if(number != null){
+                numbers.add(number);
+            }
+        }
+    }
+
+    public List<T> sort()
+    {
+        if(numbers == null){
+            throw new RuntimeException("List is null");
+        }
+        Collections.sort(this.numbers);
+        return this.numbers;
+    }
+}
---
+++ src/test/java/br/edu/fatec/sjc/sortNumber/NumberAscOrderTest.java
@@ -0,0 +1,55 @@
+package br.edu.fatec.sjc.sortNumber;
+
+import br.edu.fatec.sjc.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class NumberAscOrderTest {
+
+    private CustomStack<Double> stack;
+    @Mock
+    CalculableStrategy<Double> calculableStrategy;
+
+    NumberAscOrder<Double> numberAscOrder;
+
+    @BeforeEach
+    public void setup() {
+        stack = new CustomStack<>(6, calculableStrategy);
+    }
+
+    @Test
+    public void sortNumberAsc() throws NoSuchFieldException, IllegalAccessException, StackEmptyException {
+        List<Double> valores = Arrays.asList(1.3, 4.5, 1.2, 2.3, 8.9, 1.1);
+        valores.forEach(e -> {
+            try {
+                when(calculableStrategy.calculateValue(e)).thenReturn(e);
+                stack.push(e);
+            } catch (StackFullException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        numberAscOrder = new NumberAscOrder<>(stack);
+        List<Double> resultado = numberAscOrder.sort();
+        Assertions.assertEquals(resultado.get(0),valores.get(5));
+        Assertions.assertEquals(resultado.get(5),valores.get(4));
+    }
+
+    @Test
+    public void sortNumberAscListEmpty() throws StackEmptyException {
+        numberAscOrder = new NumberAscOrder<>(stack);
+        List<Double> resultado = numberAscOrder.sort();
+        Assertions.assertTrue(resultado.isEmpty());
+    }
+
+}
